8ae592ea0eeee5153e4a55d9779decb500805b33
Steven Ma <steven.gmdf@gmail.com>
2025-01-17 15:12:27 +1000

    Predeclare type practice

--- basic.go
+++ basic.go
@@ -8,4 +8,15 @@ func main() {
 	//x = 20
 	fmt.Println(x)
 	// x = 30
+	//var i int = 20
+	//var f float32 = float32(i)
+	//fmt.Println("i = ", i)
+	//fmt.Println("f = ", f)
+
+	const value = 10
+	var i int = value
+	var f float32 = value
+	fmt.Println("i = ", i)
+	fmt.Println("f = ", f)
+
 }
